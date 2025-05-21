4766321ce4f57486bf089d0746e76bafc487313b
Ryan Lymburner <rlymbur@amazon.com>
2025-05-21 10:01:46 -0700

    Add modified tests (currently broken)

--- test/suites/integration/grpc_service_import_test.go
+++ test/suites/integration/grpc_service_import_test.go
@@ -12,6 +12,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"github.com/aws/aws-application-networking-k8s/pkg/k8s"
 	"github.com/aws/aws-application-networking-k8s/test/pkg/test"
 	gwv1 "sigs.k8s.io/gateway-api/apis/v1"
 
@@ -33,19 +34,54 @@ var _ = Describe("GRPC Service Import Test", Ordered, func() {
 	)
 
 	It("Create k8s resources", func() {
-		// Create first GRPC service (local)
+		// Create GRPC services and deployments
 		grpcDeployment1, grpcSvc1 = testFramework.NewGrpcApp(test.GrpcAppOptions{Name: "grpc-service-01", Namespace: k8snamespace})
-		policy1 = test.CreateGRPCTargetGroupPolicy(grpcSvc1)
-		testFramework.ExpectCreated(ctx, policy1)
-
-		// Create second GRPC service (exported)
 		grpcDeployment2, grpcSvc2 = testFramework.NewGrpcApp(test.GrpcAppOptions{Name: "grpc-service-02", Namespace: k8snamespace})
-		policy2 = test.CreateGRPCTargetGroupPolicy(grpcSvc2)
-		testFramework.ExpectCreated(ctx, policy2)
-		serviceExport = testFramework.CreateServiceExport(grpcSvc2)
+
+		// Create Kubernetes API Objects
+		testFramework.ExpectCreated(ctx,
+			grpcSvc1,
+			grpcDeployment1,
+			grpcSvc2,
+			grpcDeployment2,
+		)
+
+		// Create ServiceExport and ServiceImport
+		serviceExport = &anv1alpha1.ServiceExport{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      grpcSvc2.Name,
+				Namespace: grpcSvc2.Namespace,
+				Annotations: map[string]string{
+					"application-networking.k8s.aws/federation": "amazon-vpc-lattice",
+				},
+			},
+			Spec: anv1alpha1.ServiceExportSpec{
+				ExportedPorts: []anv1alpha1.ExportedPort{
+					{
+						Port:      80,
+						RouteType: "GRPC",
+					},
+				},
+			},
+		}
 		testFramework.ExpectCreated(ctx, serviceExport)
+
+		// Wait for ServiceExport to be reconciled
+		Eventually(func(g Gomega) {
+			err := testFramework.Get(ctx, k8s.NamespacedName(serviceExport), serviceExport)
+			g.Expect(err).To(BeNil())
+			g.Expect(serviceExport.Status.Conditions).ToNot(BeEmpty())
+		}).Should(Succeed())
+
+		// Create ServiceImport
 		serviceImport = testFramework.CreateServiceImport(grpcSvc2)
 		testFramework.ExpectCreated(ctx, serviceImport)
+
+		// Create TargetGroupPolicies
+		policy1 = test.CreateGRPCTargetGroupPolicy(grpcSvc1)
+		testFramework.ExpectCreated(ctx, policy1)
+		policy2 = test.CreateGRPCTargetGroupPolicy(grpcSvc2)
+		testFramework.ExpectCreated(ctx, policy2)
 
 		// Create GRPCRoute with weighted distribution
 		grpcRoute = &gwv1.GRPCRoute{
@@ -69,7 +105,7 @@ var _ = Describe("GRPC Service Import Test", Ordered, func() {
 								BackendRef: gwv1.BackendRef{
 									BackendObjectReference: gwv1.BackendObjectReference{
 										Name: gwv1.ObjectName(grpcSvc1.Name),
-										Port: lo.ToPtr(gwv1.PortNumber(50051)),
+										Port: lo.ToPtr(gwv1.PortNumber(80)),
 									},
 									Weight: lo.ToPtr(int32(50)),
 								},
@@ -79,7 +115,7 @@ var _ = Describe("GRPC Service Import Test", Ordered, func() {
 									BackendObjectReference: gwv1.BackendObjectReference{
 										Name: gwv1.ObjectName(grpcSvc2.Name),
 										Kind: lo.ToPtr(gwv1.Kind("ServiceImport")),
-										Port: lo.ToPtr(gwv1.PortNumber(50051)),
+										Port: lo.ToPtr(gwv1.PortNumber(80)),
 									},
 									Weight: lo.ToPtr(int32(50)),
 								},
@@ -90,18 +126,12 @@ var _ = Describe("GRPC Service Import Test", Ordered, func() {
 			},
 		}
 
-		// Create Kubernetes API Objects
-		testFramework.ExpectCreated(ctx,
-			grpcSvc1,
-			grpcDeployment1,
-			grpcSvc2,
-			grpcDeployment2,
-			grpcRoute,
-		)
+		// Create GRPCRoute
+		testFramework.ExpectCreated(ctx, grpcRoute)
 	})
 
 	It("Verify lattice resources", func() {
-		// Get target groups for both services
+		// Get GRPC target groups for both services
 		localTG := testFramework.GetTargetGroupWithProtocol(ctx, grpcSvc1, vpclattice.TargetGroupProtocolHttp, vpclattice.TargetGroupProtocolVersionGrpc)
 		importedTG := testFramework.GetTargetGroupWithProtocol(ctx, grpcSvc2, vpclattice.TargetGroupProtocolHttp, vpclattice.TargetGroupProtocolVersionGrpc)
 
@@ -137,7 +167,7 @@ var _ = Describe("GRPC Service Import Test", Ordered, func() {
 		Expect(err).To(BeNil())
 		Expect(importedTags.Tags[model.K8SProtocolVersionKey]).To(Equal(aws.String(vpclattice.TargetGroupProtocolVersionGrpc)))
 
-		// Verify targets are healthy for both target groups
+		// Verify targets are healthy
 		Eventually(func(g Gomega) {
 			// Check local service targets
 			localTargets := testFramework.GetTargets(ctx, localTG, grpcDeployment1)
--- test/suites/integration/grpc_serviceexport_test.go
+++ test/suites/integration/grpc_serviceexport_test.go
@@ -9,10 +9,12 @@ import (
 	. "github.com/onsi/gomega"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/aws/aws-application-networking-k8s/test/pkg/test"
 
 	anv1alpha1 "github.com/aws/aws-application-networking-k8s/pkg/apis/applicationnetworking/v1alpha1"
+	"github.com/aws/aws-application-networking-k8s/pkg/k8s"
 	model "github.com/aws/aws-application-networking-k8s/pkg/model/lattice"
 )
 
@@ -27,38 +29,48 @@ var _ = Describe("GRPC ServiceExport Test", Ordered, func() {
 	It("Create k8s resources", func() {
 		// Create GRPC service and deployment
 		grpcDeployment, grpcSvc = testFramework.NewGrpcApp(test.GrpcAppOptions{Name: "my-grpc-1", Namespace: k8snamespace})
-		policy = test.CreateGRPCTargetGroupPolicy(grpcSvc)
-		testFramework.ExpectCreated(ctx, policy)
-		serviceExport = testFramework.CreateServiceExport(grpcSvc)
-		testFramework.ExpectCreated(ctx, serviceExport)
 
 		// Create Kubernetes API Objects
 		testFramework.ExpectCreated(ctx,
 			grpcSvc,
 			grpcDeployment,
 		)
+
+		// Create ServiceExport
+		serviceExport = &anv1alpha1.ServiceExport{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      grpcSvc.Name,
+				Namespace: grpcSvc.Namespace,
+				Annotations: map[string]string{
+					"application-networking.k8s.aws/federation": "amazon-vpc-lattice",
+				},
+			},
+			Spec: anv1alpha1.ServiceExportSpec{
+				ExportedPorts: []anv1alpha1.ExportedPort{
+					{
+						Port:      80,
+						RouteType: "GRPC",
+					},
+				},
+			},
+		}
+		testFramework.ExpectCreated(ctx, serviceExport)
+
+		// Wait for ServiceExport to be reconciled
+		Eventually(func(g Gomega) {
+			err := testFramework.Get(ctx, k8s.NamespacedName(serviceExport), serviceExport)
+			g.Expect(err).To(BeNil())
+			g.Expect(serviceExport.Status.Conditions).ToNot(BeEmpty())
+		}).Should(Succeed())
+
+		// Create TargetGroupPolicy
+		policy = test.CreateGRPCTargetGroupPolicy(grpcSvc)
+		testFramework.ExpectCreated(ctx, policy)
 	})
 
 	It("Verify lattice resources", func() {
-		// Get both HTTP and GRPC target groups
-		httpTG := testFramework.GetTargetGroupWithProtocol(ctx, grpcSvc, vpclattice.TargetGroupProtocolHttp, vpclattice.TargetGroupProtocolVersionHttp1)
+		// Get GRPC target group
 		grpcTG := testFramework.GetTargetGroupWithProtocol(ctx, grpcSvc, vpclattice.TargetGroupProtocolHttp, vpclattice.TargetGroupProtocolVersionGrpc)
-
-		// Verify HTTP target group
-		httpTGDetails, err := testFramework.LatticeClient.GetTargetGroup(&vpclattice.GetTargetGroupInput{
-			TargetGroupIdentifier: aws.String(*httpTG.Id),
-		})
-		Expect(httpTGDetails).To(Not(BeNil()))
-		Expect(err).To(BeNil())
-		Expect(*httpTG.VpcIdentifier).To(Equal(os.Getenv("CLUSTER_VPC_ID")))
-		Expect(*httpTG.Protocol).To(Equal(vpclattice.TargetGroupProtocolHttp))
-
-		// Get HTTP target group tags to verify protocol version
-		httpTags, err := testFramework.LatticeClient.ListTagsForResource(&vpclattice.ListTagsForResourceInput{
-			ResourceArn: httpTG.Arn,
-		})
-		Expect(err).To(BeNil())
-		Expect(httpTags.Tags[model.K8SProtocolVersionKey]).To(Equal(aws.String(vpclattice.TargetGroupProtocolVersionHttp1)))
 
 		// Verify GRPC target group
 		grpcTGDetails, err := testFramework.LatticeClient.GetTargetGroup(&vpclattice.GetTargetGroupInput{
@@ -76,15 +88,8 @@ var _ = Describe("GRPC ServiceExport Test", Ordered, func() {
 		Expect(err).To(BeNil())
 		Expect(grpcTags.Tags[model.K8SProtocolVersionKey]).To(Equal(aws.String(vpclattice.TargetGroupProtocolVersionGrpc)))
 
-		// Verify targets are healthy for both target groups
+		// Verify targets are healthy
 		Eventually(func(g Gomega) {
-			// Check HTTP target group targets
-			httpTargets := testFramework.GetTargets(ctx, httpTG, grpcDeployment)
-			for _, target := range httpTargets {
-				g.Expect(*target.Port).To(BeEquivalentTo(grpcSvc.Spec.Ports[0].TargetPort.IntVal))
-				g.Expect(*target.Status).To(Equal(vpclattice.TargetStatusHealthy))
-			}
-
 			// Check GRPC target group targets
 			grpcTargets := testFramework.GetTargets(ctx, grpcTG, grpcDeployment)
 			for _, target := range grpcTargets {
